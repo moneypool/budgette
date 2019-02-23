d5f8cda34c91c9293d20475638e26cd557798206
Oscar Elizondo <oscarmarcelo95@gmail.com>
2019-02-23 16:22:23 -0600

    adds budget by category query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @expenses_by_category = @user.bank_account.expenses.group_by(&:category)
-
-    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
-    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
-    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
-    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
-    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
+    @expenses_by_category = Expense.by_categories_for(@user)
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,10 @@
 class Expense < ApplicationRecord
   belongs_to :bank_account
+
+  def self.by_categories_for(user)
+    Expense
+      .select('category as category, sum(amount) as budget')
+      .where(bank_account: user.bank_account)
+      .gruop('category')
+  end
 end
