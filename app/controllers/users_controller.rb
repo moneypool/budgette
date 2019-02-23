a2f3bd7acc331d58ec52923674432bd2f9d8a711
alvaro <alv.rsl@outlook.com>
2019-02-23 15:36:42 -0600

    Making a better world

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @expenses = @user&.bank_account.expenses
-    # @goals = @user&.goals
+    @expenses_by_category = @user.bank_account.expenses.group_by(&:category)
+
+    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
+    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
+    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
+    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
+    @food_expenses = @expenses_by_category['Food'].reduce(0) {|sum, expense| sum + expense.amount }
   end
 end
