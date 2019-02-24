b8438dfad495c8d7b7a5cc62d08e6f4aa774a300
Oscar Elizondo <oscarmarcelo95@gmail.com>
2019-02-24 11:37:29 -0600

    refactors goals new

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,5 +2,5 @@ class Goal < ApplicationRecord
   belongs_to :user
 
   validates :category, presence: true
-  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :amount, presence: true
 end
