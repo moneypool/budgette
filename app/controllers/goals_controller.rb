82e190eff41c7f9d44e1730bd990358884d0fe34
Ana Karen Beltrán <anakarenbmak@yahoo.com>
2019-02-23 19:28:45 -0600

    add many goals/budgets at once

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,47 +1,41 @@
 class GoalsController < ApplicationController
   def new
-    # @goal_list = []
-    # 2.times do
-    #   @goal_list << Goal.new
-    @goal = Goal.new
-   # end
+    init_goal_list
   end
 
   def create
-    # params["goal_list"].each do |goal|
-    #   # Goal.create(goal_params(goal))
-    #   @goal = Goal.new(goal_params(goal))
-    #
-    #   unless @goal.save
-    #     render :show and return
-    #   end
-    # end
-    # render :show
-
-    @goal = Goal.new(goal_params)
-
-    if @goal.save
-      redirect_to new_group_path
-    else
-      render :new
+    params["goal_list"].each do |g|
+      @goal = Goal.new(goal_params(g))
+      unless @goal.save
+        init_goal_list
+        render :new
+        return
+      end
     end
+    redirect_to new_group_path
+  end
+
+  def init_goal_list
+    @goal_list = []
+    2.times do
+      @goal_list << Goal.new
+   end
   end
 
   def convert_percentage_to_number(user_amount)
     user_amount.to_f * current_user.monthly_income/100
   end
 
-  def default_params
-    {
-      user: current_user,
-      amount: convert_percentage_to_number(params[:goal][:amount])
-    }
+  def goal_params(individual_params)
+    individual_params.
+    permit(:category, :amount).
+    merge(default_params(individual_params))
   end
 
-  def goal_params
-    params.
-    require(:goal).
-      permit(:category, :amount).
-      merge(default_params)
+  def default_params(individual_params)
+    {
+      user: current_user,
+      amount: convert_percentage_to_number(individual_params[:amount])
+    }
   end
 end
