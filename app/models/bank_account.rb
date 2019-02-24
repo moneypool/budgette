359c9455eaf86313273274eb00f06a28224bd0dc
Oscar Elizondo <oscarmarcelo95@gmail.com>
2019-02-24 11:41:16 -0600

    removes unnecesary code from bank account

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -5,20 +5,4 @@ class BankAccount < ApplicationRecord
   belongs_to :user
   has_many :expenses
   has_many :incomes
-
-  def connect!
-    true
-  end
-
-  def generate_balance
-    true
-  end
-
-  private
-
-  def generate_incomes
-  end
-
-  def generate_expenses
-  end
 end
